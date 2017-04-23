4bc7671973bb79dbf93d82593c784ef01f7bfbf4
SpyR1014 <spyr1014@gmail.com>
2017-04-24 00:24:42 +1000

    definiteions file init

---
+++ networkvizjs/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for networkVizJS 0.0.3
+// Project: networkVizJS
+// Definitions by: Andrew Jakubowicz
+
+/**
+ * Declaration help file found here:
+ * https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+ */
+
+// global UMD module definition
+export as namespace networkVizJS;
+
+
+
+// /*~ If there are types, properties, or methods inside dotted names
+//  *~ of the module, declare them inside a 'namespace'.
+//  */
+// export namespace subProp {
+//     /*~ For example, given this definition, someone could write:
+//      *~   import { subProp } from 'yourModule';
+//      *~   subProp.foo();
+//      *~ or
+//      *~   import * as yourMod from 'yourModule';
+//      *~   yourMod.subProp.foo();
+//      */
+//     export function foo(): void;
+// }
