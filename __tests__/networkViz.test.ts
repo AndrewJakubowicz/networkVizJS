cf9f0dc6971a476c6820879087562e187188de8e
SpyR1014 <spyr1014@gmail.com>
2017-06-02 13:40:57 +1000

    added tests to check initialisation

--- __tests__/networkViz.test.ts
+++ __tests__/networkViz.test.ts
@@ -3,14 +3,33 @@
  */
 /// <reference types="jest" />
 
-import * as n from "../src/networkViz";
+import networkViz from "../src/networkViz";
 import * as jsdom from 'jsdom';
+var n = require('../src/networkViz');
 
-it("Should import as a function", () => {
-    var networkViz = require('../src/networkViz');
-    expect(networkViz).toHaveProperty("default");
-    expect(networkViz.default).toEqual(expect.any(Function));
+describe("Importing", function(){
+    it("Should import as a function", () => {
+        // Demonstrates require import path.
+        expect(n).toHaveProperty("default");
+        expect(n.default).toEqual(expect.any(Function));
+        expect(networkViz).toEqual(expect.any(Function));
+    });
 });
+
+describe("Embedding", function(){
+    it("SVG embed on div - id", function(){
+        document.body.innerHTML = `<div id="testId">
+        </div>`;
+        expect(document.getElementsByTagName("svg").length).toEqual(0);
+        expect(document.getElementsByClassName("svg-container").length).toBeFalsy();
+        networkViz("testId");
+        expect(document.getElementsByTagName("svg").length).toEqual(1);
+        console.log(document.body.innerHTML);
+        expect(document.getElementsByClassName("svg-container").length).toBeTruthy();
+        expect((<SVGElement>document.getElementsByTagName("svg")[0]).getAttribute("viewBox").split(/\s+|,/)).toEqual(["0", "0", "900", "600"]);
+    });
+});
+
 
 
 
