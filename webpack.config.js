588f3c13ccdec9704fc9f2c5c2cce97e93825870
Yusuf Ades <yusuf.ades@monash.edu>
2021-04-29 02:51:52 +1000

    add bundling option

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
 const path = require("path");
 const webpack = require("webpack")
 const include = path.join(__dirname, 'src')
-
+console.log(__dirname)
 module.exports = {
-    entry: './src/networkViz',
+    entry: './dist/networkViz',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         libraryTarget: 'umd',
         library: 'networkVizJS',
+        filename: 'networkviz.js',
     },
     devtool: 'source-map',
-    module: {
-    loaders: [
-        {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['env']
-            }
-        }
+    mode: 'development',
+    resolve: {
+        modules: [
+            path.join(__dirname, 'node_modules')
         ]
     },
     externals: {
-        d3: 'd3',
-        webcola: 'cola'
+        // d3: 'd3',
+        // webcola: 'cola'
     }
 }
